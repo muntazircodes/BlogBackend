d94b7a3ca370acbabda989ace1342e7a2a0d4228
mutazircodes <muntazircodes@gmail.com>
2025-02-04 20:26:13 +0530

    The auth services has created for now

---
+++ services/authServices.js
@@ -0,0 +1,33 @@
+const User = require("../models/Users");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (userData) => {
+  const { name, email, password } = userData;
+
+  // Check if user exists
+  const existingUser = await User.findOne({ email });
+  if (existingUser) throw new Error("User already exists");
+
+  // Create new user
+  const newUser = new User({ name, email, password });
+  await newUser.save();
+
+  return newUser;
+};
+
+const loginUser = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) throw new Error("Invalid email or password");
+
+  // Compare passwords
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) throw new Error("Invalid email or password");
+
+  // Generate JWT token
+  const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+  return { token, user };
+};
+
+module.exports = { registerUser, loginUser };
