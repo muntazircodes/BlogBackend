ce1b50d8afce8494d9706b3f955b6c02e2916b05
mutazircodes <mutazircodes@gmail.com>
2025-02-04 15:04:12 +0530

    Added few routes

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+const corsOptions = {
+  origin: process.env.ALLOWED_ORIGIN || "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+};
+app.use(cors(corsOptions));
+
+app.use("/api/users", userRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong!" });
+});
 
 module.exports = app;
