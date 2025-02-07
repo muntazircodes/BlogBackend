9c7690131b1bc6d4067c452ebfecf82803caa090
mutazircodes <muntazircodes@gmail.com>
2025-02-07 12:07:08 +0530

    Just reformated the code

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,10 @@ import dotenv from "dotenv";
 import connectDB from "./config/db";
 import app from "./app";
 
-// Load environment variables
 dotenv.config();
-
-// Connect to database
 connectDB();
 
-// Define the port with a fallback
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
-// Start the server
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => 
+    console.log(`🚀 Server running on http://localhost:${PORT}`));
